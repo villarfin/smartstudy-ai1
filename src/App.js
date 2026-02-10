e92524d8519f7275ab25c1d428b906fc04cd42db
Zachariah Zane Villar <villar.zachariahzane@gmail.com>
2026-02-11 00:51:42 +0800

    Initial SmartStudy AI React setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Login from "./Login";
 function App() {
   return (
     <main>
+      <h2 style={{ textAlign: "center", color: "red" }}>
+        SmartStudy AI – LIVE
+      </h2>
       <Login />
     </main>
   );
